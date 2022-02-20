bb877665283aeaaadb3634c8fcc2f0dee33d03bc
xzdraz12 <xzdraz12@vutbr.cz>
2022-02-20 23:22:56 +0100

    v1.1 (2. verze)

--- CV3/Subject.java
+++ CV3/Subject.java
@@ -1,7 +1,38 @@
+import java.util.Scanner;
 public class Subject
 {
     public String Subject_name;
     public int Subject_points;
     public int Minimal_Points = 15;
+    static Scanner sc;
+    public int yes;
+}
+
+public static void BPC1 ()
+{
+    system.out.println("Chcete zadat body ze cvičení (0) nebo ze zkoušky (1)?");
+    Scanner sc = new Scanner(System.in);
+    yes = sc.nextInt();
+    if (yes == 0)
+    {
+        Exercises_Points();
+    }
+    else if (yes == 1)
+    {
+        Exam_Points();
+    }
+    else ()
+    {
+        system.out.println("Zadali jste spatne cislo");
+    }
+}
+
+
+private static void Exercises_Points ()
+{
+
+}
+private static void Exam_Points()
+{
 
 }
