8682eac48426dec9e3f1808e731221441420815b
xzdraz12 <xzdraz12@vutbr.cz>
2022-02-23 11:46:02 +0100

    v1.1 (2. verze)

---
+++ CV3/SubjectBPC_pis.java
@@ -0,0 +1,14 @@
+public class SubjectBPC_pis implements Databasecv3
+{
+    private int Body;
+    public boolean problem;
+
+
+    @Override
+    public String getSubject_name (){return "BPIS"};
+
+    @Override
+    public int getPoints(){return Body};
+
+
+}
